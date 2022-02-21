d6a7016136f915047ec213f7716fd73f5bb2f97b
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-21 16:07:54 +0600

    All calculation done

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 /* $$$$$$ Total Net Price $$$$$$ */
 const netPrice = document.getElementById("total-price");
 
+onClick("eightGB", "memory-cost", 00);
+onClick("sixteenGB", "memory-cost", 300);
+onClick("ssd1", "storage-cost", 00);
+onClick("ssd2", "storage-cost", 200);
+onClick("ssd3", "storage-cost", 500);
+onClick("free-delivery", "delivery-cost", 00);
+onClick("paid-delivery", "delivery-cost", 20);
 
-document.getElementById("sixteenGB").addEventListener("click", () => {
-   
-    updateBalance('memory-cost', 200)
-});
+/* ============== Common Function to fire Click Event ============== */
+function onClick(button, input, price) {
+  document.getElementById(button).addEventListener("click", () => {
+    updateBalance(input, price);
+  });
+}
 
-/* ============== Common Function to Update Balance  */
+/* ============== Common Function to Update Balance ============== */
 function updateBalance(input, price) {
-    let itemCost = document.getElementById(input);
-    itemCost.innerText = price;
-  
-    const memoryCost = document.getElementById("memory-cost");
-    const storageCost = document.getElementById("storage-cost");
-    const deliveryCost = document.getElementById("delivery-cost");
-  
-    const totalCost =
-      parseInt(memoryCost.innerText) + parseInt(storageCost.innerText) + parseInt(deliveryCost.innerText);
-  
-    netPrice.innerText = parseFloat(netPrice.innerText) + totalCost;
+  let itemCost = document.getElementById(input);
+  itemCost.innerText = price;
+
+  const bestPrice = document.getElementById("best-price");
+  const memoryCost = document.getElementById("memory-cost");
+  const storageCost = document.getElementById("storage-cost");
+  const deliveryCost = document.getElementById("delivery-cost");
+
+  const totalCost =
+    parseFloat(bestPrice.innerText) +
+    parseInt(memoryCost.innerText) +
+    parseInt(storageCost.innerText) +
+    parseInt(deliveryCost.innerText);
+
+  netPrice.innerText = totalCost;
 }
